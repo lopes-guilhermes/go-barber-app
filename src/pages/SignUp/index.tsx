db84f197e30377d4a73e5b0a678c58f12c6927d6
Guilherme Lopes <guilhermelopes.silva@hotmail.com>
2021-05-13 13:18:52 -0300

    form signIn e signUp

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { 
   Image, 
   View, 
@@ -8,6 +8,9 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -22,6 +25,7 @@ import {
 } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
 
   return (
@@ -41,11 +45,17 @@ const SignUp: React.FC = () => {
               <Title>Crie sua conta</Title>
             </View>
 
-            <Input name="name" icon="user" placeholder="Nome" />
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
-            
-            <Button onPress={() => {console.log('SignUp');}}>Entrar</Button>
+            <Form ref={formRef} onSubmit={(data) => {
+              console.log(data);
+            }} style={{width: '100%'}}>
+              <Input name="name" icon="user" placeholder="Nome" />
+              <Input name="email" icon="mail" placeholder="E-mail" />
+              <Input name="password" icon="lock" placeholder="Senha" />
+              
+              <Button onPress={() => {
+                formRef.current?.submitForm();
+              }}>Entrar</Button>
+            </Form>
 
           </Container>
         </ScrollView>
