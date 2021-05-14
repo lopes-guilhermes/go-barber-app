e3521915e865cbf5c9e94bdcce29ee1b4e4edaee
Guilherme Lopes <guilhermelopes.silva@hotmail.com>
2021-05-13 23:49:49 -0300

    autenticação no app

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -14,6 +14,7 @@ interface SignInCredentials {
 
 interface AuthContextData {
   user: object;
+  loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -22,6 +23,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStoragedData(): Promise<void> {
@@ -36,6 +38,8 @@ const AuthProvider: React.FC = ({ children }) => {
           user: JSON.parse(user[1])
         });
       }
+
+      setLoading(false);
     }
 
     loadStoragedData();
@@ -64,7 +68,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
@@ -80,4 +84,4 @@ function useAuth(): AuthContextData {
   return context;
 }
 
-export { AuthProvider, useAuth }; 
+export { AuthProvider, useAuth,  }; 
