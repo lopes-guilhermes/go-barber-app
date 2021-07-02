374fa0cb15594d4b7a30f8beeacedd238370d9b9
Guilherme Lopes <guilhermelopes.silva@hotmail.com>
2021-07-02 12:42:21 -0300

    header CreateAppointment

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,9 +1,45 @@
+import { useNavigation, useRoute } from "@react-navigation/native";
 import React from "react";
-import { View } from "react-native";
+import { useCallback } from "react";
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useAuth } from "../../hooks/auth";
+
+import { 
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar
+} from "./styles";
+
+interface RouteParms {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
+  const route = useRoute();
+  const { user } = useAuth();
+  const { goBack } = useNavigation();
+  
+  const { providerId } = route.params as RouteParms;
+  
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack])
+
   return (
-    <View/>
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
   );
 };
 
